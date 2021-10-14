868dc2dba422486ab5588e4122d62cdc1dc97a1f
Qamar Riaz <qamarriaz9@gmail.com>
2021-10-14 19:18:50 +0500

    Added Page object model classes Login and Home page

---
+++ Pages/HomePage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpNetCore.Pages
+{
+    public class HomePage : DriverHelper
+    {
+        IWebElement lnkLogin => driver.FindElement(By.LinkText("Login"));
+        IWebElement lnkLogOff => driver.FindElement(By.LinkText("Log off"));
+
+        public void ClickLogin() => lnkLogin.Click();
+
+        public bool IsLogOffExist() => lnkLogOff.Displayed;
+    }
+}
---
+++ Pages/LoginPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpNetCore.Pages
+{
+    public class LoginPage: DriverHelper
+    {
+
+        IWebElement txtUserName => driver.FindElement(By.Id("UserName"));
+        IWebElement txtPassword => driver.FindElement(By.Id("Password"));
+        IWebElement btnLogin => driver.FindElement(By.CssSelector(".btn-default"));
+
+        public void EnterUserNameAndPassword(string username, string password)
+        {
+            txtUserName.SendKeys(username);
+            txtPassword.SendKeys(password);
+        }
+        public void ClickLoginButton()
+        {
+            btnLogin.Click();
+        }
+      
+    }
+}
